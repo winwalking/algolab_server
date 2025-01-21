d94533fcdf7ea8fbdc62270efe6a31365c428b48
100jinah <winwalking1106@gmail.com>
2025-01-21 16:37:59 +0900

    20250121 server first commit re fixed3

--- api/index.py
+++ api/index.py
@@ -62,5 +62,5 @@ def send_email():
         app.logger.error(f"Error occurred: {e}")
         return jsonify({"message": "Failed to send email", "error": str(e)}), 500
 
-if __name__ == "__main__":
-    app.run(debug=True)
+# if __name__ == "__main__":
+#     app.run(debug=True)
